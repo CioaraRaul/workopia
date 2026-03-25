8df6ee4beb7c2420e3cea7147be6733807b0f821
Cioara Raul <cioararaul08@gmail.com>
2026-03-25 23:17:50 +0200

    start with laravel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function(){
+    return 'Available jobs';
+})->name('jobs');
+
+Route::match(['get','post'],'/submit', function(){
+    return 'Submitted';
+});
+
